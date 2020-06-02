20d57545972dbbbd2ba0ba02a0314454c8d22144
Erica <ericajself@gmail.com>
2020-06-01 20:09:36 -0400

    Finished

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+    "nav-item-7": "Cookies",
+    "nav-item-8": "Cake",
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -37,6 +39,60 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+// Example: Update the img src for the logo, make nav text green
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+// navbar links, also appending 2 links in to navbar
+let navBar = document.querySelector("nav");
+navBar.appendChild( document.createElement("a"));
+navBar.prepend( document.createElement("a"));
+console.log(navBar);
+for (let i = 0; i < navBar.children.length; i++){
+  navBar.children[i].textContent = siteContent.nav["nav-item-" + (i + 1)];
+  navBar.children[i].style.color = "green";
+}
+
+// navbar logo
+document.getElementById("logo-img").src = siteContent.nav["img-src"];
+
+// h1
+document.querySelector("h1").textContent = siteContent.cta["h1"];
+
+// button
+document.querySelector("button").textContent = siteContent.cta["button"];
+
+// cta image
+document.getElementById("cta-img").src = siteContent.cta["img-src"];
+
+// textcontent
+let temp = ["features", "about", "services", "product", "vision"];
+let i = 0;
+temp.forEach(element => {
+  document.getElementsByClassName("text-content")[i].children[0].textContent = siteContent["main-content"][element + "-h4"];
+  document.getElementsByClassName("text-content")[i].children[1].textContent = siteContent["main-content"][element + "-content"];
+  i++;
+});
+
+// mid image
+document.getElementById("middle-img").src = siteContent["main-content"]["middle-img-src"];
+
+// contact h4
+document.getElementsByClassName("contact")[0].children[0].textContent = siteContent["contact"]["contact-h4"];
+
+// contact address
+document.getElementsByClassName("contact")[0].children[1].textContent = siteContent["contact"]["address"];
+
+// contact phone
+document.getElementsByClassName("contact")[0].children[2].textContent = siteContent["contact"]["phone"];
+
+// contact email
+document.getElementsByClassName("contact")[0].children[3].textContent = siteContent["contact"]["email"];
+
+// footer
+document.querySelector("footer").children[0].textContent = siteContent["footer"]["copyright"];
+
+// task 3 stuff, refer to line 46 loop
+
+
+
